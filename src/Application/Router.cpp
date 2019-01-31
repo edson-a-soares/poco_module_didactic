37ce7552a6000cea63244ca51f3561a64c7fbfad
edsonsoares <edson.araujo.soares@gmail.com>
2019-01-31 07:23:13 -0300

    Refactor for general improvement

--- src/Application/Router.cpp
+++ src/Application/Router.cpp
@@ -2,7 +2,6 @@
 #include "Poco/ClassLibrary.h"
 #include "Application/Router.h"
 #include "Poco/Net/HTTPServerRequest.h"
-#include "Application/Resource/RouteNotFound.h"
 #include "Application/Resource/Factory/AbstractFactory.h"
 
 namespace Application {
@@ -37,6 +36,7 @@ namespace Application {
 
 }
 
+// add support to Poco ApacheConnector
 POCO_BEGIN_MANIFEST(Poco::Net::HTTPRequestHandlerFactory)
     POCO_EXPORT_CLASS(Application::Router)
 POCO_END_MANIFEST
